2e8c17435e9bc6b844559929c27e4649df706659
Vipul Kumar <vipul.kumar@taazaa.com>
2023-09-03 11:34:22 +0530

    first commit to psh

---
+++ src/main/java/com/vipulkdev/psh/PshApplication.java
@@ -0,0 +1,13 @@
+package com.vipulkdev.psh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vipulkdev/psh/PshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipulkdev.psh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
